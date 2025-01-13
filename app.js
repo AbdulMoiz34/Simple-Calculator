bd5889bb05e6e8b3136d8136796411baf94844a1
Abdul Moiz <moiz05366@gmail.com>
2025-01-13 19:51:07 +0500

    added history-tracking feature

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+let historyArr = [];
+
 function calculator(newVal) {
     const calcInp = document.getElementById("inp");
     const operators = ["*", "/", "%", "+", "-", "."];
     const lastChar = calcInp.value.slice(-1);
     // console.log(lastChar);
     if (newVal == "=") {
+        history(calcInp.value);
         calcInp.value = eval(calcInp.value);
     } else if (newVal == "DEL") {
         calcInp.value = calcInp.value.slice(0, - 1);
@@ -14,4 +17,30 @@ function calculator(newVal) {
     } else {
         calcInp.value += newVal;
     }
+}
+
+// history
+function history(val) {
+    const historyBoxEl = document.querySelector(".history-box>div");
+    historyBoxEl.innerHTML = "";
+    historyArr.push(val);
+    for (let i = 0; i < historyArr.length; i++) {
+        historyBoxEl.innerHTML += `<button onclick="appendVal(${i})">${historyArr[i]}</button>`;
+    }
+    // console.log(historyArr);
+}
+
+function toggleHistory() {
+    const historyBoxEl = document.querySelector(".history-box");
+    historyBoxEl.classList.toggle("hidden");
+    if (!historyArr.length) {
+        historyBoxEl.innerHTML = "<div>There is no history yet.<div>";
+        return;
+    }
+}
+
+function appendVal(i) {
+    const newVal = historyArr[i];
+    const calcInp = document.getElementById("inp");
+    calcInp.value += newVal;
 }
